2bf26bc6b0fad2cce06ef1e5f4c5d661a97af655
mijinB <bmj13465@gmail.com>
2025-01-04 19:10:52 +0900

    front한테 code 받아서 access token 받는 kakao api 호출(WebClient 사용 & Service로 메서드 이동)

--- src/main/java/planpad/planpadapp/service/UserService.java
+++ src/main/java/planpad/planpadapp/service/UserService.java
@@ -1,17 +1,53 @@
 package planpad.planpadapp.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 import planpad.planpadapp.domain.User;
+import planpad.planpadapp.dto.user.TokenResponseDto;
+import planpad.planpadapp.dto.user.UserRequestDto;
 import planpad.planpadapp.repository.UserRepository;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public TokenResponseDto getAccessToken(UserRequestDto request) {
+
+        WebClient webClient = WebClient.create();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+        String CLIENT_ID = "97106bc8684af5584543581289cfd304";
+        String REDIRECT_URI = "http://localhost:3000/api/auth/kakao";
+        String code = request.getCode();
+
+        String response = webClient.post()
+                .uri(TOKEN_URL)
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+                .bodyValue("grant_type=authorization_code" +
+                        "&client_id=" + CLIENT_ID +
+                        "&redirect_uri=" + REDIRECT_URI +
+                        "&code=" + code)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();       // 동기적으로 응답 값을 반환
+
+        try {
+            return objectMapper.readValue(response, TokenResponseDto.class);
+        } catch (JsonProcessingException e) {
+            log.info("(log) JSON 처리 중 오류 발생: {}", e.getMessage());
+            throw new RuntimeException("JSON 처리 중 오류 발생: " + e.getMessage(), e);
+        }
+    }
 
     @Transactional
     public Long join(User user) {
