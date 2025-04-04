9ca6871ac690a52f905b1cfd54b9a3839f66cb7f
Bmijin <bmj13465@jtts.co.kr>
2025-04-04 14:40:58 +0900

    주석 제거

--- src/main/java/planpad/planpadapp/service/memo/MemoService.java
+++ src/main/java/planpad/planpadapp/service/memo/MemoService.java
@@ -93,10 +93,4 @@ public class MemoService {
             }
         }
     }
-
-    /*List<Tag> tags = dtoData.getTags().stream()
-            .map(tag -> tagRepository.findByName(tag.getName())
-                    .orElseGet(() -> tagRepository.save(new Tag(tag.getName()))))
-            .collect(Collectors.toList());
-            이렇게 tags를 변경하고 memo 생성자에 넣어서 생성하기*/
 }
